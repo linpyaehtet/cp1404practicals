9cde4fea34457e84dbead36e594a2087d3358258
linpyaehtet <pyaehtet.lin@my.jcu.edu.au>
2024-09-29 22:45:03 +0800

    Mention to the user that the password has to be at least 8 characters or more

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -7,9 +7,9 @@ display '*' * length of password
 MINIMUM_LENGTH = 8
 
 def main():
-    password = input("Enter password: ")
+    password = input(f"Enter password ({MINIMUM_LENGTH} characters or more): ")
     while len(password) < MINIMUM_LENGTH:
-        password = input("Enter password: ")
+        password = input(f"Enter password ({MINIMUM_LENGTH} characters or more): ")
     print('*' * len(password))
 
 
