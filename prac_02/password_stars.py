f89a38e133ecd9dc2b8db7c04f8be7da2c866838
linpyaehtet <pyaehtet.lin@my.jcu.edu.au>
2024-09-29 22:37:27 +0800

    Turning pseudocode into program

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,4 +2,9 @@
 get password
 while password < 8
     get password
+display '*' * length of password
 """
+password = input("Enter password: ")
+while len(password) < 8:
+    password = input("Enter password: ")
+print('*' * len(password))
