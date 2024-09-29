81c8525dbfc6509ccf45c760f6560a64ceb524ac
linpyaehtet <pyaehtet.lin@my.jcu.edu.au>
2024-09-29 23:16:27 +0800

    Turns the part where it takes in the user's score and returns result into a function

--- prac_02/score.py
+++ prac_02/score.py
@@ -7,12 +7,16 @@ def main():
     score = float(input("Enter score: "))
     print(determine_result(score))
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-        print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+
+def determine_result(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
