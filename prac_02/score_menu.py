6a9a20ad21e319a5d76bb1d5c259fc7141635f61
linpyaehtet <pyaehtet.lin@my.jcu.edu.au>
2024-09-29 23:32:17 +0800

    Write main program

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,1 +1,22 @@
 MENU = "(G)et a valid score (must be 0-100 inclusive) \n(P)rint result \n(S)how stars \n(Q)uit"
+GET_SCORE_OPTION = 'G'
+PRINT_RESULT_OPTION = 'P'
+SHOW_STAR_OPTION = 'S'
+QUIT_OPTION = 'Q'
+
+def main():
+    print(MENU)
+    choice = input("Please enter your option: ")
+    while choice != QUIT_OPTION:
+        if choice == GET_SCORE_OPTION:
+            score = get_valid_score()
+        elif choice == PRINT_RESULT_OPTION:
+            print(determine_result(score))
+        elif choice == SHOW_STAR_OPTION:
+            display_asterisks()
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input("Please enter your option: ")
+
+
