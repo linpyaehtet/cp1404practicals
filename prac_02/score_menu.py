49c835ad3559c01c66f48f12b42b7fb814a9549b
linpyaehtet <pyaehtet.lin@my.jcu.edu.au>
2024-09-29 23:37:15 +0800

    Write determine_result() function

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,8 +1,12 @@
 MENU = "(G)et a valid score (must be 0-100 inclusive) \n(P)rint result \n(S)how stars \n(Q)uit"
+
 GET_SCORE_OPTION = 'G'
 PRINT_RESULT_OPTION = 'P'
 SHOW_STAR_OPTION = 'S'
 QUIT_OPTION = 'Q'
+
+EXCELLENT_THRESHOLD = 90
+PASSABLE_THRESHOLD = 50
 
 def main():
     print(MENU)
@@ -10,10 +14,13 @@ def main():
     while choice != QUIT_OPTION:
         if choice == GET_SCORE_OPTION:
             score = get_valid_score()
+            print()
         elif choice == PRINT_RESULT_OPTION:
             print(determine_result(score))
+            print()
         elif choice == SHOW_STAR_OPTION:
             display_asterisks()
+            print()
         else:
             print("Invalid option")
         print(MENU)
@@ -26,6 +33,15 @@ def get_valid_score():
         print("Invalid score")
         score = float(input("Enter your score: "))
     return score
+
+
+def determine_result(score):
+    if score >= EXCELLENT_THRESHOLD:
+        return "Excellent"
+    elif score >= PASSABLE_THRESHOLD:
+        return "Passable"
+    else:
+        return "Bad"
 
 
 main()
