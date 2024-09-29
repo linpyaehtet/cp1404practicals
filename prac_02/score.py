a929d5a68098f016d321563864b8915398909e76
linpyaehtet <pyaehtet.lin@my.jcu.edu.au>
2024-09-29 23:21:59 +0800

    Turning magic numbers into constants

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,20 +3,24 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 import random
+MINIMUM_SCORE = 0
+MAXIMUM_SCORE = 100
+EXCELLENT_THRESHOLD = 90
+PASSABLE_THRESHOLD = 50
 
 def main():
     score = float(input("Enter score: "))
     print(determine_result(score))
-    random_score = random.randint(0, 100)
+    random_score = random.randint(MINIMUM_SCORE, MAXIMUM_SCORE)
     print(f"Random generated score is {random_score} and the result is {determine_result(random_score)}")
 
 
 def determine_result(score):
-    if score < 0 or score > 100:
+    if score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
         return "Invalid score"
-    elif score >= 90:
+    elif score >= EXCELLENT_THRESHOLD:
         return "Excellent"
-    elif score >= 50:
+    elif score >= PASSABLE_THRESHOLD:
         return "Passable"
     else:
         return "Bad"
