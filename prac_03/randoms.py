a096552458244f70b5384a9d46eeb45ff8342c2a
linpyaehtet <pyaehtet.lin@my.jcu.edu.au>
2024-10-06 18:54:53 +0800

    Print random number between 1 and 100 inclusive

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -15,3 +15,6 @@
 # What was the smallest number you could have seen, what was the largest?
 # The smallest number was 2.5 and the largest was 5.5.
 # Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+
+import random
+print(random.randint(1, 100))
