f4d03eeab2cd20ae4cb842dfe0b32e3b10caca99
linpyaehtet <pyaehtet.lin@my.jcu.edu.au>
2024-10-06 20:44:14 +0800

    Complete question 3

--- prac_03/files.py
+++ prac_03/files.py
@@ -9,3 +9,10 @@ in_file = open('name.txt', 'r')
 name_from_file = in_file.readline().strip()
 print(f"Hi {name_from_file}!")
 in_file.close()
+
+# Question 3
+with open('numbers.txt', 'r') as in_file:
+    number_1 = int(in_file.readline())
+    number_2 = int(in_file.readline())
+    result = number_1 + number_2
+    print(result)
