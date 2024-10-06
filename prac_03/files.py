5268fada1084335a190a30395b8dbe3b09b5bc7a
linpyaehtet <pyaehtet.lin@my.jcu.edu.au>
2024-10-06 20:39:53 +0800

    Complete question 2

--- prac_03/files.py
+++ prac_03/files.py
@@ -4,3 +4,8 @@ out_file = open('name.txt', 'w')
 print(name, file=out_file)
 out_file.close()
 
+# Question 2
+in_file = open('name.txt', 'r')
+name_from_file = in_file.readline().strip()
+print(f"Hi {name_from_file}!")
+in_file.close()
