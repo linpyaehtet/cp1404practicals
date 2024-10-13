bdbccd85c2ff6b8cc2345195eae882615a156ca0
linpyaehtet <pyaehtet.lin@my.jcu.edu.au>
2024-10-13 23:47:07 +0800

    Add docstring explaining what the function does

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -31,6 +31,7 @@ def load_data():
 
 
 def display_subject_details(data):
+    """Display subject details similar to the sample provided"""
     for i in range(len(data)):
         print(f"{data[i][0]} is taught by {data[i][1]:12} and has {data[i][2]:3} students")
 
