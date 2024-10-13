49780483b0822a2a2bd2a7d5c84f708733bfc116
linpyaehtet <pyaehtet.lin@my.jcu.edu.au>
2024-10-13 23:50:57 +0800

    Add docstring to explain the print_report() functoin

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -17,6 +17,7 @@ def main():
 
 
 def print_report(incomes):
+    "Display income report with the format: Month - Income - Total"
     print("\nIncome Report\n-------------")
     total = 0
     for month in range(1, len(incomes)+1):
