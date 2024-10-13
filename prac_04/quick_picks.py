fe3cee79c5f84b2d266bc014dec63eb5d3e9e61c
linpyaehtet <pyaehtet.lin@my.jcu.edu.au>
2024-10-13 23:48:12 +0800

    Generate many lines of output consisting 6 random numbers between 1 and 45

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -5,3 +5,12 @@ MINIMUM = 1
 MAXIMUM = 45
 
 number_of_quick_picks = int(input("How many quick picks? "))
+for i in range(number_of_quick_picks):
+    random_numbers = []
+    for j in range(RANDOM_NUMBER_AMOUNT):
+        number = random.randint(MINIMUM, MAXIMUM)
+        while number in random_numbers:
+            number = random.randint(MINIMUM, MAXIMUM)
+        random_numbers.append(number)
+    random_numbers.sort()
+    print(" ".join(f"{number:2}" for number in random_numbers))
