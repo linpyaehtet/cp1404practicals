e77e493a15eaab0d1aa86cf20795d9dd7f4ecb3c
linpyaehtet <pyaehtet.lin@my.jcu.edu.au>
2024-10-13 23:11:55 +0800

    Complete all the print statements

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -3,3 +3,8 @@ numbers = []
 for i in range(TOTAL_NUMBER):
     number = int(input("Number: "))
     numbers.append(number)
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f"The average of the numbers is {sum(numbers)/TOTAL_NUMBER}")
