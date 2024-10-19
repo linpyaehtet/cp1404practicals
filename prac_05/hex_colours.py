e40952b1f9254e2c18fbac6dbaa37c3ca1213940
linpyaehtet <pyaehtet.lin@my.jcu.edu.au>
2024-10-20 05:07:31 +0800

    Add a constant dictionary of 10 colour names

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,2 @@
+COLOUR_TO_HEXADECIMAL = {"aliceblue": "#b0bf1a", "black": "#000000", "pink": "#ffc0cb", "ruby": "#e0115f", "lavender": "#e6e6fa",
+                         "iris": "#5a4fcf", "indigo": "#4b0082", "floralwhite": "#fffaf0", "electricblue": "#7df9ff", "deeppeach": "#ffcba4"}
