171c05816c7b819d88876350a5742103f61a1886
linpyaehtet <pyaehtet.lin@my.jcu.edu.au>
2024-10-20 04:59:52 +0800

    Remove the original LBYL approach

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -11,12 +11,6 @@ print(CODE_TO_NAME)
 
 state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
-        print("Invalid short state")
-    state_code = input("Enter short state: ").upper()
-
     # EAFP Approach
     try:
         print(state_code, "is", CODE_TO_NAME[state_code])
