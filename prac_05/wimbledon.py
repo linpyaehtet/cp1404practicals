d8a448549ba4b257cd04b1fd50e54327b220acfd
linpyaehtet <pyaehtet.lin@my.jcu.edu.au>
2024-10-20 15:10:30 +0800

    Create a dictionary of champion as key and count of the wins as value

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,7 +1,20 @@
+FILENAME = 'wimbledon.csv'
+CHAMPION_INDEX = 2
+CHAMPION_COUNTRY_INDEX = 1
+
 def main():
-    filename = 'wimbledon.csv'
-    with open(filename, "r", encoding="utf-8-sig") as in_file:
-        lines = [line.strip() for line in in_file.readlines()]
+    champion_to_count = {}
+    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()
+        entries = [line.strip().split(",") for line in in_file.readlines()]
+    print(entries)
+
+    for entry in entries:
+        champion = entry[CHAMPION_INDEX]
+        champion_to_count[champion] = champion_to_count.get(champion, 0) + 1
+    print("Wimbledon Champions:")
+    for champion, count in champion_to_count.items():
+        print(f"{champion} {count}")
 
 
 main()
