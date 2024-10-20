3523182a7c5ad40c5ff9ed13cc502424b49dd5f4
linpyaehtet <pyaehtet.lin@my.jcu.edu.au>
2024-10-20 15:31:38 +0800

    Refactor loading data into a function

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -2,19 +2,21 @@ FILENAME = 'wimbledon.csv'
 CHAMPION_INDEX = 2
 CHAMPION_COUNTRY_INDEX = 1
 
+champion_to_count = {}
+countries = set()
+
 def main():
-    champion_to_count = {}
+    """Read csv file and print the results of winners and their countries"""
+    entries = load_data()
+    print(entries)
+
+
+def load_data():
+    """Load data into a list of lists"""
     with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
         in_file.readline()
         entries = [line.strip().split(",") for line in in_file.readlines()]
-    print(entries)
-
-    for entry in entries:
-        champion = entry[CHAMPION_INDEX]
-        champion_to_count[champion] = champion_to_count.get(champion, 0) + 1
-    print("Wimbledon Champions:")
-    for champion, count in champion_to_count.items():
-        print(f"{champion} {count}")
+    return entries
 
 
 main()
