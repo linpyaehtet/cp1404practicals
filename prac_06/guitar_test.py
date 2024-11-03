abc3550d88352ead1b9953acd58715a85502f158
linpyaehtet <pyaehtet.lin@my.jcu.edu.au>
2024-11-03 22:21:46 +0800

    Test get_age() and is_vintage() methods with two guitars

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,7 @@
+from prac_06.guitar import Guitar
+first_guitar = Guitar("Gibson L-5 CES", 1922)
+second_guitar = Guitar("Another Guitar", 2013)
+print(f"{first_guitar.name} get_age() - Expected 100. Got {second_guitar.get_age()}")
+print(f"{second_guitar.name} get_age() - Expected 9. Got {second_guitar.get_age()}")
+print(f"{first_guitar.name} is_vintage() - Expected True. Got {second_guitar.is_vintage()}")
+print(f"{second_guitar.name} is_vintage() - Expected False. Got {second_guitar.is_vintage()}")
