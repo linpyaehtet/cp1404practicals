60f09bdfcbb1c473e7e039f2e93d4ee3b0cc3870
linpyaehtet <pyaehtet.lin@my.jcu.edu.au>
2024-11-03 22:06:00 +0800

    Define get_age() method and add a CURRENT_YEAR constant

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,3 +1,4 @@
+CURRENT_YEAR = 2024
 class Guitar:
     def __init__(self, name="", year=0, cost=0):
         self.name = name
@@ -6,3 +7,6 @@ class Guitar:
 
     def __str__(self):
         return f"{self.name} ({self.year}) : {self.cost}"
+
+    def get_age(self):
+        return CURRENT_YEAR - self.year
