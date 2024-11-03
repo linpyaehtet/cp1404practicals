e2f74030d9994b8f3c07e81571ea4d941fd36067
linpyaehtet <pyaehtet.lin@my.jcu.edu.au>
2024-11-03 22:48:07 +0800

    Print the output

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -14,3 +14,10 @@ while name != "":
     guitars.append(Guitar(name, year, cost))
     print()
     name = input("Name: ")
+
+print()
+
+print("These are my guitars:")
+for i, guitar in enumerate(guitars, 1):
+    vintage_string = "vintage" if guitar.is_vintage() else ""
+    print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f} ({vintage_string})")
