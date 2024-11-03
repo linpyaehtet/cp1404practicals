04d3b041acb6be25c0111a402b8ebfc4cb6cbfdf
linpyaehtet <pyaehtet.lin@my.jcu.edu.au>
2024-11-03 22:47:38 +0800

    Get user input

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -5,3 +5,12 @@ guitars = []
 guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
 guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
+print("My guitars!")
+name = input("Name: ")
+while name != "":
+    year = int(input("Year: "))
+    cost = float(input("Cost: $"))
+    print(f"{name} ({year}) : ${cost:.2f} added.")
+    guitars.append(Guitar(name, year, cost))
+    print()
+    name = input("Name: ")
