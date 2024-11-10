ad1b419ebd15377dd5d685ac9a56ce1d0b8a290e
linpyaehtet <pyaehtet.lin@my.jcu.edu.au>
2024-11-10 22:26:43 +0800

    Sort projects by priority

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,9 +1,13 @@
 class Project:
-    def __init__(self, name="", start_date="", priority=0, cost_estimate=float(0), completion_percentage=0):
+    def __init__(self, name="", date="", priority=0, cost_estimate=0, completion=0):
         self.name = name
-        self.start_date = start_date
+        self.date = date
         self.priority = priority
         self.cost_estimate = cost_estimate
-        self.completion_percentage = completion_percentage
+        self.completion = completion
 
     def __str__(self):
+        return f"{self.name}, start: {self.date}, priority: {self.priority}, estimate: ${self.cost_estimate:.2f}, completion: {self.completion}%"
+
+    def __lt__(self, other):
+        return self.priority < other.priority
