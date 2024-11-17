2714c4c8aa8dfa66819545f0cfde42b47b313662
linpyaehtet <pyaehtet.lin@my.jcu.edu.au>
2024-11-17 19:49:22 +0800

    Add docstrings

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -4,9 +4,11 @@ from kivy.uix.label import Label
 from kivy.properties import StringProperty
 
 class DynamicLabelsApp(App):
+    """App to dynamically create Labels from a list of names."""
     status_text = StringProperty()
 
     def __init__(self, **kwargs):
+        """Initialize list."""
         super().__init__(**kwargs)
         self.names = ["Lin Pyae", "Zizi", "Poypoy"]
 
@@ -18,6 +20,7 @@ class DynamicLabelsApp(App):
         return self.root
 
     def create_labels(self):
+        """Create a label for each name and add to the layout."""
         for name in self.names:
             temp_label = Label(text=name)
             self.root.ids.main.add_widget(temp_label)
