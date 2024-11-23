134296c24d86bd7a1895530a6fce6982f2fcae60
linpyaehtet <pyaehtet.lin@my.jcu.edu.au>
2024-11-24 03:27:22 +0800

    Make proper print statement

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -2,5 +2,5 @@ from prac_09.silver_service_taxi import SilverServiceTaxi
 
 fancy_taxi = SilverServiceTaxi("Hummer", 100, 2)
 fancy_taxi.drive(18)
-print(f"Fare: {fancy_taxi.get_fare()}")
+print(f"Fare: ${fancy_taxi.get_fare()}")
 print(fancy_taxi)
