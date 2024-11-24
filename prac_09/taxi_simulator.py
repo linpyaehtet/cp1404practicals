eef427d0210f63df3abb531e22e934d2edf4cbd3
linpyaehtet <pyaehtet.lin@my.jcu.edu.au>
2024-11-24 12:39:31 +0800

    Reformat menu into constant

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -3,7 +3,8 @@ from prac_09.silver_service_taxi import SilverServiceTaxi
 
 current_taxi = None
 taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+MENU = "q)uit, c)hooose taxi, d)rive"
 
 print("Let's drive!")
-print("q)uit, c)hooose taxi, d)rive")
+print(MENU)
 user_choice = input(">>> ")
