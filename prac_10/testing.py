344986388fbc29fd54e6160c4037a7ffca6a0552
linpyaehtet <pyaehtet.lin@my.jcu.edu.au>
2024-12-01 23:36:03 +0800

    Complete testing.py

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -73,7 +73,6 @@ doctest.testmod()
 
 # (Don't change the tests, change the function!)
 
-# TODO: 5. Write and test a function to format a phrase as a sentence,
 # starting with a capital and ending with a single full stop.
 # Important: start with a function header and just use pass as the body
 # then add doctests for 3 tests:
